36af7ed61dd79cb64fc14621ef20e9b19a3604b4
Ny'alotha <Nyalotha@outlook.com>
2017-11-14 17:59:19 +0200

    added ReportsPerPubblicazione and DataTable jQuery plugin

--- QPortal/Controllers/ReportsController.cs
+++ QPortal/Controllers/ReportsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QPortal.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,39 @@ namespace QPortal.Controllers
         public ActionResult CreateReport()
         {
             return View("CreateReport");
+        }
+
+        //GET: Pubblicazione
+        public ActionResult Pubblicazione()
+        {
+            List<string> streams = new List<string>();
+            streams.Add("Stream 1");
+            streams.Add("Stream 2");
+            streams.Add("Stream 3");
+
+            Report one = new Report();
+            one.Owner = "Giacomo Guilizzoni";
+            one.ReportName = "Founder & CEO";
+
+            Report two = new Report();
+            two.Owner = "Giacomo Guilizzoni";
+            two.ReportName = "CFO";
+
+            Report three = new Report();
+            three.Owner = "Test PErson";
+            three.ReportName = "NAN";
+
+            ReportViewModel rModel = new ReportViewModel();
+            rModel.Streams = new List<string>(streams);
+
+            List<Report> rList = new List<Report>();
+            rList.Add(one);
+            rList.Add(two);
+            rList.Add(three);
+            rModel.Reports = rList;
+
+            //string partialView = "_ReportPerPubblicazione";
+            return View("ReportPerPubblicazione",rModel);
         }
 
     }
---
+++ QPortal/ViewModels/ReportViewModel.cs
@@ -0,0 +1,21 @@
+﻿using QPortal.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QPortal.ViewModels
+{
+    public class Report
+    {
+        public string Owner { get; set; }
+        public string ReportName { get; set; }
+        public bool Selected { get; set; }
+    }
+
+    public class ReportViewModel
+    {
+        public List<string> Streams { get; set; }
+        public List<Report> Reports { get; set; }
+    }
+}
