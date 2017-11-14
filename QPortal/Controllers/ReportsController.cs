865e82861258b0c35b16ce717813de6b1c46a1b3
Ny'alotha <Nyalotha@outlook.com>
2017-11-14 14:34:35 +0200

    added Reports - Create

---
+++ QPortal/Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QPortal.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CreateReport
+        public ActionResult CreateReport()
+        {
+            return View("CreateReport");
+        }
+
+    }
+}
