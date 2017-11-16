9514fe69462e8a9b61775fa9ef763a12a958f8aa
Ny'alotha <Nyalotha@outlook.com>
2017-11-16 18:01:35 +0200

    ajax rendering of DataTable and css

--- QPortal/Controllers/ReportsController.cs
+++ QPortal/Controllers/ReportsController.cs
@@ -29,25 +29,28 @@ namespace QPortal.Controllers
             streams.Add("Stream 2");
             streams.Add("Stream 3");
 
+            Report zero = new Report();
+            zero.Id = 0;
+            zero.Owner = "Giacomo Guilizzoni";
+            zero.ReportName = "Founder & CEO";
+
             Report one = new Report();
+            one.Id = 1;
             one.Owner = "Giacomo Guilizzoni";
-            one.ReportName = "Founder & CEO";
+            one.ReportName = "CFO";
 
             Report two = new Report();
-            two.Owner = "Giacomo Guilizzoni";
-            two.ReportName = "CFO";
-
-            Report three = new Report();
-            three.Owner = "Test PErson";
-            three.ReportName = "NAN";
+            two.Id = 2;
+            two.Owner = "Test PErson";
+            two.ReportName = "NAN";
 
             ReportViewModel rModel = new ReportViewModel();
             rModel.Streams = new List<string>(streams);
 
             List<Report> rList = new List<Report>();
+            rList.Add(zero);
             rList.Add(one);
             rList.Add(two);
-            rList.Add(three);
             rModel.Reports = rList;
 
             //string partialView = "_ReportPerPubblicazione";
